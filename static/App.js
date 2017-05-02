84725f8b3d8058a80d2b6ff9776e8634ae3fa63e
Sean Lin <sean.linxs@gmail.com>
2017-05-02 10:34:14 +1000

    Move data layer into API

--- static/App.js
+++ static/App.js
@@ -184,17 +184,6 @@ var IssueAdd = function (_React$Component2) {
   return IssueAdd;
 }(React.Component);
 
-var issues = [{
-  id: 1, status: 'Open', owner: 'Ravan',
-  created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
-  title: 'Error in console when clicking Add'
-}, {
-  id: 2, status: 'Assigned', owner: 'Eddie',
-  created: new Date('2016-08-16'), effort: 14,
-  completionDate: new Date('2016-08-30'),
-  title: 'Missing bottom border on panel'
-}];
-
 var IssueList = function (_React$Component3) {
   _inherits(IssueList, _React$Component3);
 
