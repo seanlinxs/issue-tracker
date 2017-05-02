570c969e8451b7a15685baa2f4d51658d9c439b6
Sean Lin <sean.linxs@gmail.com>
2017-05-02 20:10:26 +1000

    Add webpack-dev-server proxy for API

--- webpack.config.js
+++ webpack.config.js
@@ -26,5 +26,14 @@ module.exports = {
         }
       },
     ]
+  },
+  devServer: {
+    port: 8000,
+    contentBase: 'static',
+    proxy: {
+      '/api/*': {
+        target: 'http://localhost:3000'
+      }
+    }
   }
 };
