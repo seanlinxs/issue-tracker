f448b9806c26bc2acb140edee8d91165fdd7bb43
Sean Lin <sean.linxs@gmail.com>
2017-05-04 22:08:37 +1000

    Add server bundle

---
+++ webpack.server-config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  target: 'node',
+  entry: './server/server.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'server.bundle.js',
+    libraryTarget: 'commonjs',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  externals: [/^[a-z]/],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015-node4'],
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015-node4'],
+        },
+      },
+    ],
+  },
+  devtool: 'source-map',
+};
