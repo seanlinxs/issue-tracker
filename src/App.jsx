4f676955cba33b799e0ffc6f9838720bcd9ac2e0
Sean Lin <sean.linxs@gmail.com>
2017-05-05 12:57:58 +1000

    Add search UI using react-select

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,19 @@
-import PropTypes from 'prop-types';
 import React from 'react';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Glyphicon } from 'react-bootstrap';
 import ReactDOM from 'react-dom';
-import { LinkContainer } from 'react-router-bootstrap';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import IssueList from './IssueList.jsx';
 import IssueEdit from './IssueEdit.jsx';
 import IssueReport from './IssueReport.jsx';
-import IssueAddNavItem from './IssueAddNavItem.jsx';
-import withToast from './withToast.jsx';
+import Header from './Header.jsx';
 
 const contentNode = document.getElementById('contents');
 
 const NoMatch = () => <p>Page Not Found</p>;
 
-const Header = props => (
-  <Navbar fluid>
-    <Navbar.Header>
-      <Navbar.Brand>Issue Tracker</Navbar.Brand>
-    </Navbar.Header>
-    <Nav>
-      <LinkContainer to="/issues">
-        <NavItem>Issues</NavItem>
-      </LinkContainer>
-      <LinkContainer to="/reports">
-        <NavItem>Reports</NavItem>
-      </LinkContainer>
-    </Nav>
-    <Nav pullRight>
-      <IssueAddNavItem showError={props.showError} />
-      <NavDropdown
-        id="user-dropdown"
-        title={<Glyphicon glyph="option-horizontal" />}
-        noCaret
-      >
-        <MenuItem>Logout</MenuItem>
-      </NavDropdown>
-    </Nav>
-  </Navbar>
-);
-
-Header.propTypes = {
-  showError: PropTypes.func.isRequired,
-};
-
-const HeaderWithToast = withToast(Header);
-
 const App = () => (
   <Router>
     <div>
-      <HeaderWithToast />
+      <Header />
       <div className="container-fluid">
         <Switch>
           <Route exact path="/issues" component={IssueList} />
